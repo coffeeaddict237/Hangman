72bb8f086127d0e233ba51ae96eb4c9787725664
coffeeaddict237 <saragl@bu.edu>
2018-02-25 19:50:41 -0500

    keyboard fix

--- app/src/main/java/com/example/saraglattstein/hangman/MainActivity.java
+++ app/src/main/java/com/example/saraglattstein/hangman/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.saraglattstein.hangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_1;
+    private TextView tv_2;
+    private TextView tv_3;
+    private TextView tv_4;
+    private TextView tv_5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_1 = findViewById(R.id.tv_1);
+        tv_2 = findViewById(R.id.tv_2);
+        tv_3 = findViewById(R.id.tv_3);
+        tv_4 = findViewById(R.id.tv_4);
+        tv_5 = findViewById(R.id.tv_5);
+
+        NewGame hang = new NewGame();
+
     }
 }
--- app/src/main/java/com/example/saraglattstein/hangman/NewGame.java
+++ app/src/main/java/com/example/saraglattstein/hangman/NewGame.java
@@ -4,5 +4,50 @@ package com.example.saraglattstein.hangman;
  * Created by saraglattstein on 2/25/18.
  */
 
+import java.util.*;
+
 public class NewGame {
+
+    private String hint;
+    private String answer;
+    private String[] animals = {"dog", "cat", "horse", "fish", "snake", "bird", "lion", "owl", "mouse", "sheep"};
+    private String[] food = {"apple", "pear", "peach", "melon", "lemon", "lime", "grape", "mango", "kiwi", "plum"};
+
+    public NewGame() {
+        //random select animals/food & random select within list
+        if(Math.random() > 0.5) {
+            hint = "animals";
+            answer = animals[(int) (Math.random() * 10)];
+        }
+        else {
+            hint = "food";
+            answer = food[(int) (Math.random() * 10)];
+        }
+
+    }
+
+    public String getHint() { return hint;}
+
+    public String getAnswer() {return answer;}
+
+    public int getLength() {return answer.length();}
+
+    public boolean guess(char input) {
+        if(answer.indexOf(input) != -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public List getIndex(char input) {
+        List<Integer> places = new ArrayList<>();
+        for(int i = 0; i < answer.length(); i++) {
+            if(guess(input)) {
+                places.add(answer.indexOf(input));
+            }
+        }
+        return places;
+    }
+
+
 }
